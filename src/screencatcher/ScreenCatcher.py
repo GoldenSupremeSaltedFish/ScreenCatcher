dbdc81ded97a0c6bd757a69bddb9311db9371f93
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2024-09-22 18:36:37 +0800

    PDM: Perform a CI check for source code
    - Reformat code with Ruff
    - Lint code with Ruff
    - Static type check with Mypy
      - Add some "magic comments" back for supressing warnings
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- src/screencatcher/ScreenCatcher.py
+++ src/screencatcher/ScreenCatcher.py
@@ -1,29 +1,31 @@
 import threading
 import tkinter as tk
 from tkinter import scrolledtext
-from PIL import ImageGrab
 import webbrowser
-from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode  # type: ignore[import-untyped]
 import time
-from ScreenShot import ScreenCapture
+from .ScreenShot import ScreenCapture
 import keyboard  # 快捷键监听模块
 
 
 def debounce(wait):
     def decorator(fn):
         last_call = [0]
+
         def debounced(*args, **kwargs):
             now = time.time()
             if now - last_call[0] >= wait:
                 last_call[0] = now
                 return fn(*args, **kwargs)
+
         return debounced
+
     return decorator
 
 
 @debounce(0.5)  # 防抖，0.5秒间隔
 def recognizeQrCode():
-     # 保证截图窗口在主线程中运行
+    # 保证截图窗口在主线程中运行
     capture_thread = ScreenCapture()
 
     # 获取截图结果，阻塞直到有结果或超时
@@ -36,17 +38,18 @@ def recognizeQrCode():
     # img.show()
     threading.Thread(target=process_qr_code, args=(img,), daemon=True).start()
 
+
 # 处理二维码识别
 def process_qr_code(img):
     qrCodes = decode(img)
     resultTextbox.delete(1.0, tk.END)  # 清空文本框
     for qrCode in qrCodes:
-        qrData = qrCode.data.decode('utf-8')
+        qrData = qrCode.data.decode("utf-8")
         resultTextbox.insert(tk.END, f"识别到的二维码信息: {qrData}\n")
 
-        if qrData.startswith('http://weixin.qq.com'):
+        if qrData.startswith("http://weixin.qq.com"):
             resultTextbox.insert(tk.END, f"这是微信的链接，目前无法支持打开 {qrData}\n")
-        elif qrData.startswith('http'):
+        elif qrData.startswith("http"):
             resultTextbox.insert(tk.END, f"正在打开浏览器: {qrData}\n")
             webbrowser.open(qrData)
             resultTextbox.insert(tk.END, "浏览器已打开\n")
@@ -56,10 +59,12 @@ def process_qr_code(img):
     if not qrCodes:
         resultTextbox.insert(tk.END, "未识别到二维码\n")
 
+
 # 快捷键监听函数
 def start_keyboard_listener():
-    keyboard.add_hotkey('alt+q', recognizeQrCode)  # 监听 Alt+Q 组合键
-    keyboard.wait('esc')  # 按 Esc 键退出程序
+    keyboard.add_hotkey("alt+q", recognizeQrCode)  # 监听 Alt+Q 组合键
+    keyboard.wait("esc")  # 按 Esc 键退出程序
+
 
 # 创建GUI
 def create_gui():
@@ -78,13 +83,15 @@ def create_gui():
 
     root.mainloop()
 
+
 # 主函数
 def main():
     # 创建线程来运行快捷键监听
     keyboard_thread = threading.Thread(target=start_keyboard_listener, daemon=True)
     keyboard_thread.start()
-    
+
     create_gui()
+
 
 if __name__ == "__main__":
     main()
